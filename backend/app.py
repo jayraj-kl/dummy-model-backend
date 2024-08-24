cf342628e94477a38ba6ea5cd804da18c7831f8b
Kandyyy <kingkandy016@gmail.com>
2024-08-24 13:35:01 +0530

    removed auto-delete

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,6 @@ import os
 def predict():
     preprocessed_img = preprocess_data("t8.png")
     prediction = get_prediction(preprocessed_img)
-    if os.path.exists("./t8.png"):
-        os.remove("./t8.png")
     print(prediction)
 
 predict()
